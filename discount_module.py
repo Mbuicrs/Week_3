184bf31723d3a4ae5c1b11dea98cfd4482092c92
Cyrus <mbuicyrus@gmail.com>
2024-03-07 13:11:48 +0300

    first commit

---
+++ discount_module.py
@@ -0,0 +1,15 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * (discount_percent / 100))
+        return discounted_price
+    else:
+        return price
+
+
+# Prompt the user to enter the original price and discount percentage
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+# output the final price
+price = calculate_discount(original_price, discount_percent)
+print("\t", "Your bill will be: ", price)
